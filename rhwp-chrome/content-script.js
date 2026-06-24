6538a6b7d7923fc02b52e9b1b39075da5b3600ae
edward <tangokorea@gmail.com>
2026-06-25 00:14:58 +0900

    chore: 브라우저 확장 0.2.8 배포 준비

--- rhwp-chrome/content-script.js
+++ rhwp-chrome/content-script.js
@@ -123,9 +123,9 @@
 
   // 확장 존재 알림
   document.documentElement.setAttribute('data-hwp-extension', 'rhwp');
-  document.documentElement.setAttribute('data-hwp-extension-version', '0.2.7');
+  document.documentElement.setAttribute('data-hwp-extension-version', '0.2.8');
   window.dispatchEvent(new CustomEvent('hwp-extension-ready', {
-    detail: { name: 'rhwp', version: '0.2.7', capabilities: ['preview', 'edit', 'print'] }
+    detail: { name: 'rhwp', version: '0.2.8', capabilities: ['preview', 'edit', 'print'] }
   }));
 
   // 개발자 도구 주입 (페이지 컨텍스트에 rhwpDev 노출)
